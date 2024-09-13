b5b69e981b9ccdf772d45541bdc8112452507e86
Gabor Liptak <liptakgabor@freemail.hu>
2024-09-13 19:06:58 +0200

    gitk script for lilen is added

---
+++ _gitk_lilen.bat
@@ -0,0 +1,7 @@
+::set path=C:\Programs\Git\bin\;%PATH%
+::@set path=C:\Programs\cygwin\bin\;%PATH%
+@set path=C:\Programok\Git\bin;%PATH%
+::@set path=C:\Programs\cygwin\bin\;%PATH%
+@set HOME=C:\Users\liptakok\Gabor
+::gitk
+C:\Programok\Git\cmd\git-gui.exe
